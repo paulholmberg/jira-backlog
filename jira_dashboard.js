b87997430e102c78b6afa9a7c9db101d97e14717
Paul Holmberg <paul.f.holmberg@gmail.com>
2019-09-20 14:07:06 +1000

    Deploying libraries to public github repository

---
+++ jira_dashboard.js
@@ -0,0 +1,465 @@
+function formatObjTable(table, exclude, cls) {
+    var str = [];
+    for (var p in table) {
+        if (table.hasOwnProperty(p) && exclude.indexOf(p) < 0) {
+            str.push('<td><b>' + p + ': </b></td><td>' + [table[p]].join('') + '</td>');
+        }
+    }
+    str = '<table class="' + cls + '" style=color:#fff><tr>' + str.join('</tr><tr>') + '</tr></table>';
+    return str;
+}
+
+function get_issue_data(jira_data) {
+    backlog = jira_data.issues.map(function(issue) {
+        return [{
+            x: 0,
+            y: issue.fields.customfield_10262 != null ? issue.fields.customfield_10262 : 10,
+            Key: issue.key,
+            Summary: issue.fields.summary,
+            "Story Points": issue.fields.customfield_10262,
+            Version: (issue.fields.fixVersions && issue.fields.fixVersions.length > 0) ? issue.fields.fixVersions[0].name : "",
+            Epic: issue.fields.epic ? issue.fields.epic.name : ""
+        }]
+    });
+
+    var cumsum = 0;
+    var epics = {};
+    backlog.map(function(issue) {
+        issue = issue[0];
+        var key = issue.Epic + " (" + issue.Version + ")";
+        if (!epics.hasOwnProperty(key)) {
+            epics[key] = {
+                start: cumsum,
+                end: cumsum + issue.y,
+                epic: issue.Epic,
+                version: issue.Version
+            };
+        } else {
+            epics[key].end = cumsum + issue.y;
+        }
+        cumsum += issue.y;
+    });
+
+    var epic_list = [];
+    for (key in epics) {
+        if (epics.hasOwnProperty(key)) {
+            epic_list.push({
+                key: key,
+                start: epics[key].start,
+                end: epics[key].end,
+                idx: idx,
+                epic: epics[key].epic,
+                version: epics[key].version
+            });
+        }
+    }
+
+    epic_list.sort(function(a, b) {
+        return a.start - b.start
+    });
+    for (var idx = 0; idx < epic_list.length; idx++) {
+        epic_list[idx].idx = idx;
+    }
+
+    return [backlog, epic_list];
+}
+
+function analyse_issues(issues) {
+    var breakdown = {
+        epics: {},
+        versions: {}
+    };
+    issues.map(function(issue) {
+        issue = issue[0];
+        breakdown.epics[issue.Epic] = (breakdown.epics[issue.Epic] || 0) + issue.y;
+        breakdown.versions[issue.Version] = (breakdown.versions[issue.Version] || {});
+        breakdown.versions[issue.Version][issue.Epic] = (breakdown.versions[issue.Version][issue.Epic] || 0) + issue.y;
+        breakdown.versions[issue.Version].Total = (breakdown.versions[issue.Version].Total || 0) + issue.y;
+    })
+
+    for (var p in breakdown.versions) {
+        if (breakdown.versions.hasOwnProperty(p)) {
+            blank = breakdown.versions[p][''];
+            delete breakdown.versions[p][''];
+            if (blank > 0) {
+                breakdown.versions[p]['No epic'] = blank;
+            }
+
+            total = breakdown.versions[p].Total;
+            delete breakdown.versions[p].Total
+            breakdown.versions[p].Total = total;
+        }
+    }
+
+    return breakdown;
+}
+
+function plot_jira(target, jira_data, velocity, startDate) {
+    var backlog, epic_list;
+    [backlog, epic_list] = get_issue_data(jira_data);
+
+    if (!velocity) {
+        velocity = 30;
+    }
+    if (!startDate) {
+        startDate = d3.time.thursday(new Date());
+        startDate.setDate(startDate.getDate() + 7);
+    }
+    var velocity_per_day = velocity / 7;
+
+    var margin = {
+            top: 20,
+            right: 450,
+            bottom: 30,
+            left: 50
+        };
+    width = 1200 - margin.left - margin.right;
+    height = 2000 - margin.top - margin.bottom;
+
+    var endDate = new Date(startDate);
+    endDate.setDate(endDate.getDate() + 7 * 26)
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width]);
+
+    var y = d3.time.scale()
+        .domain([startDate, endDate])
+        .range([0, height]);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(d3.time.thursdays, 2)
+        .tickFormat(d3.time.format("%b %d"));
+    
+    // Clear all plot elements
+    function clear_plot() {
+        var elements = document.getElementsByClassName('d3-tip');
+        while(elements.length > 0){
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+        document.getElementById(target).innerHTML = "";
+    }
+    clear_plot();
+
+    var svg = d3.select("#" + target).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return formatObjTable(d, ['x', 'y', 'y0'], 'tooltip')
+        })
+
+    svg.call(tip);
+
+    toDate = function(y) {
+        d = new Date(startDate);
+        return new Date(d.getTime() + y / velocity_per_day * 24 * 60 * 60000);
+    }
+
+    function get_ytransform(el)
+    {
+        if (el.attr("transform") != "")
+        {
+            return parseFloat(el.attr("transform").split(",")[1].replace(/[^\d.]/g, ''));
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    var issue_moves = [];
+    var above_issue;
+
+    var drag_y = undefined;
+    var curr_drag_index = undefined;
+    var drag = d3.behavior.drag()
+        .on("dragstart", dragstarted)
+        .on("drag", dragged)
+        .on("dragend", dragended)
+        .origin(function(d) {return {'y': get_ytransform(d3.select(this))}})
+        
+    function dragstarted(d) {
+        curr_drag_index = backlog.map(function(d){return d[0].Key;}).indexOf(d3.event.sourceEvent.target.__data__.Key);
+        d3.event.sourceEvent.stopPropagation();
+        d3.select(this).classed("dragging", true);
+        d3.select(this).selectAll("rect").classed("dragging", true);
+    }
+
+    function dragged(d) {
+        tip.hide();
+        drag_y = d3.event.y;
+        if (curr_drag_index != undefined)
+        {
+            new_index = backlog.map(function (d) {
+                  return y(toDate(d[0].y0 + d[0].y/2));
+                }).findIndex(function (d) {return d > drag_y;})
+            if (curr_drag_index != new_index)
+            {
+                jira_data.issues.move(curr_drag_index, new_index);
+                [backlog, epic_list] = get_issue_data(jira_data);
+                above_issue = backlog[new_index + 1][0].Key;
+                new_jira_data = backlog;
+                curr_drag_index = new_index;
+                plot(backlog, epic_list);
+            }
+        }
+        d3.select(this).attr("transform", "translate(0, " + (d3.event.y) + ")");
+    }
+
+    Array.prototype.move = function(from, to) {
+        this.splice(to, 0, this.splice(from, 1)[0]);
+    };
+
+    function dragended(d) {
+        d3.select(this).classed("dragging", false);
+        d3.select(this).selectAll("rect").classed("dragging", false);
+        if (above_issue != undefined)
+        {
+            issue_moves.push([backlog[curr_drag_index][0].Key, above_issue]);
+            jira_data.moves = issue_moves;
+            console.log(issue_moves)
+        }
+        plot(backlog, epic_list);
+        drag_y = undefined;
+        curr_drag_index = undefined
+        above_issue = undefined;
+    }
+
+    plot(backlog, epic_list);
+
+    function plot(data, epic_list) {
+        //svg.selectAll("g").data([]).exit().remove()
+
+        var layers = d3.layout.stack()(data).map(function (d) {return d[0]});
+        var box_marginv = 2;
+        var box_marginh = 4;
+        var epic_barwidth = 25;
+
+        var versions = [];
+        data.map(function(d) {
+            if (versions.indexOf(d[0].Version) < 0) {
+                versions.push(d[0].Version)
+            }
+        });
+        versions.sort();
+
+        var epics = [];
+        data.map(function(d) {
+            if (epics.indexOf(d[0].Epic) < 0) {
+                epics.push(d[0].Epic)
+            }
+        });
+        epics.sort();
+
+        var epic_order = [];
+        for (var i = 0; i < epic_list.length; i++) {
+            epic_order.push(epic_list[i].key);
+        }
+
+        function colors_google(n) {
+            var colors_g = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+            return colors_g[(n + colors_g.length) % colors_g.length];
+        }
+
+        x.domain(layers.map(function(d) {
+            return d.x;
+        }));
+
+        layer_base = svg.selectAll(".layer")
+            .data(layers, function(d) { return d.Key; });
+
+        layer = layer_base.enter().append("g")
+            .attr("class", "layer")
+            .attr("transform", function(d) {
+                return "translate(0, " + (y(toDate(d.y0))) + ")"
+            })
+            .call(drag);
+
+        layer_base
+            /* Don't transition items being dragged */
+            .select(function (d,i) {return d3.select(this).classed("dragging") ? null : this;})
+            .transition()
+            .attr("transform", function(d) {
+                return "translate(0, " + (y(toDate(d.y0))) + ")"
+            });
+
+        boxheight = function(d) {
+            return d3.max([1, y(toDate(d.y + d.y0)) - y(toDate(d.y0)) - box_marginv * 2]);
+        }
+
+        function addRow(d, i) {
+            d3.select(this)
+                .append("rect")
+                .attr("class", "storybox");
+
+            d3.select(this)
+                .append("text")
+                .attr("class", "storylabel");;
+
+            d3.select(this)
+                .append("rect")
+                .attr("class", "ganttboxes");
+            
+            d3.select(this)
+                .append("path")
+                .attr("class", "milestones");
+        }
+
+        function updateRow(d, i) {
+            d3.select(this)
+                .selectAll(".storybox")
+                .datum(d)
+                .attr("x", box_marginh)
+                .attr("y", box_marginv)
+                .attr("height", boxheight(d))
+                .attr("width", width)
+                .style("fill", colors_google(versions.indexOf(d.Version)))
+                .on('mouseover', function(d) {
+                    d2 = d;
+                    d['Ends on'] = toDate(d.y + d.y0).toDateString();
+                    return tip.show(d2);
+                })
+                .on('mouseout', tip.hide);
+
+            d3.select(this)
+                .selectAll(".storylabel")
+                .datum(d)
+                .attr("x", d.x + box_marginh * 2 + 10 + 5)
+                .attr("y", boxheight(d)/2 + box_marginv)
+                .attr("opacity", boxheight(d) > 14 ? 1 : 0)
+                .text(d.Key + ": " + d.Summary + " (" + (d['Story Points'] != null ? d['Story Points'] : 'unestimated') + ")");
+
+            d3.select(this)
+                .selectAll(".ganttboxes")
+                .datum(d)
+                .attr("x", function(d) {
+                    return (box_marginh * 2 + 10 + width) + box_marginh + epic_order.indexOf(d.Epic + " (" + d.Version + ")") * epic_barwidth + 2
+                })
+                .attr("y", box_marginv)
+                .attr("height", boxheight)
+                .attr("width", epic_barwidth - box_marginh - 4);
+            
+            d3.select(this)
+                .selectAll(".milestones")
+                .datum(d)
+                .attr("opacity", d.Summary.toLowerCase().startsWith('milestone:') ? 1 : 0)
+                .attr("d", d3.svg.symbol().type("diamond"))
+                .attr("transform", "translate(0, " + (boxheight(d)/2) + ")")
+                .on('mouseover', function (d) {
+                    tip.show({'Date': toDate(d.y + d.y0).toDateString()})
+                })
+                .on('mouseout', tip.hide);
+        }
+
+        layer.each(addRow)
+        layer_base.each(updateRow)
+
+        svg.selectAll(".axis").data([0]).enter().append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxis);
+
+        var epiclayers_base = svg.selectAll(".epiclayers")
+            .data(epic_list, function(d) {return d.key});
+
+        var epiclayers = epiclayers_base.enter()
+            .append("g")
+            .attr("class", "epiclayers");
+
+        epicboxheight = function(d) {
+            return d3.max([1, y(toDate(d.end)) - y(toDate(d.start)) - box_marginv * 2]);
+        }
+
+        epiclayers.append("rect")
+            .attr("class", "epicboxes")
+            .attr("x", function(d) {
+                return (box_marginh * 2 + 10 + width) + box_marginh + d.idx * epic_barwidth;
+            })
+            .attr("y", function(d) {
+                return y(toDate(d.start));
+            })
+            .attr("height", function(d) {
+                return epicboxheight(d) + box_marginv * 2
+            })
+            .attr("width", epic_barwidth - box_marginh)
+            .style("stroke", '#000')
+            .style("fill", '#fff');
+
+        epiclayers_base.select(".epicboxes").transition()
+            .attr("x", function(d) {
+                return (box_marginh * 2 + 10 + width) + box_marginh + d.idx * epic_barwidth;
+            })
+            .attr("y", function(d) {
+                return y(toDate(d.start));
+            })
+            .attr("height", function(d) {
+                return epicboxheight(d) + box_marginv * 2
+            })
+
+        epiclayers.append("text")
+            .attr("class", "epictext")
+            .text(function(d) {
+                return d.key
+            })
+            .attr("transform", function(d) {
+                return "translate(" + ((box_marginh * 2 + 10 + width) + box_marginh + d.idx * epic_barwidth + 6) + "," + (y(toDate(d.start)) + 10) + ")rotate(90)"
+            });
+
+        epiclayers_base.select(".epictext").transition()
+            .text(function(d) {
+                return d.key
+            })
+            .attr("transform", function(d) {
+                return "translate(" + ((box_marginh * 2 + 10 + width) + box_marginh + d.idx * epic_barwidth + 6) + "," + (y(toDate(d.start)) + 10) + ")rotate(90)"
+            });
+
+        var legendlayers = svg.selectAll(".legendlayers")
+            .data(versions)
+            .enter()
+            .append("g")
+            .attr("class", "legendlayers");
+
+        var legend_spacing = 150;
+        legendlayers.append("rect")
+            .attr("class", "legendboxes")
+            .attr("x", function(d) {
+                return versions.indexOf(d) * legend_spacing;
+            })
+            .attr("y", function(d) {
+                return -15;
+            })
+            .attr("height", function(d) {
+                return 10
+            })
+            .attr("width", 10)
+            .style("stroke", '#000')
+            .style("fill", function(d, i) {
+                return colors_google(versions.indexOf(d));
+            });
+
+        legendlayers.append("text")
+            .attr("class", "legendtext")
+            .attr("x", function(d) {
+                return versions.indexOf(d) * legend_spacing + 15;
+            })
+            .attr("y", function(d) {
+                return -5;
+            })
+            .text(function(d) {
+                if (d)
+                {
+                    return d;
+                }
+                else
+                {
+                    return "None"
+                }
+            });
+    }
+}
---
+++ jira_query.js
@@ -0,0 +1,129 @@
+var jiraq = {
+
+    preparedom : function (target_id) {
+        ip = document.createElement('input');
+        ip.text = 'Reload data from JIRA'
+        document.getElementById(target_id).appendChild(ip)
+    }
+
+};
+
+
+
+function jira_call(url, on_success) {
+    AJS.$.ajax({
+        url: url,
+        type: "GET",
+        async: true,
+        dataType: "json"
+    }).done(on_success).fail(function(jqXHR, textStatus) {
+        data = {
+            'textStatus': textStatus,
+            'url': url
+        };
+        alert("Request failed: " + JSON.stringify(data, null, 4));
+    });
+}
+
+
+function jira_get_all(query_url, fieldname, on_update) {
+    var updated = false;
+    var messages = [];
+    var n_issued = -1;
+    var max_results = 50;
+    
+    jira_call(query_url + "&maxResults=0", function(msg) {
+        var nvals = msg.total;
+
+        function get_single(start_at, on_update) {
+            n_issued = n_issued + 1;
+            jira_call(query_url + "&startAt=" + start_at + "&maxResults=" + max_results, function(msg) {
+                messages.push(msg);
+                messages.sort(function(a, b) {
+                    return a.startAt - b.startAt
+                });
+                var has_gaps = false;
+                for (var i = 1; i < messages.length; i++) {
+                    if (messages[i].startAt != (messages[i-1].startAt + messages[i-1][fieldname].length))
+                    {
+                        has_gaps = true;
+                    }
+                }
+                if ((messages[0].startAt == 0 && messages[messages.length - 1].startAt + messages[messages.length - 1].issues.length >= nvals) && !updated && !has_gaps) {
+                    updated = true;
+                    console.log("got all " + nvals + " results, last total was " + messages[messages.length - 1].total);
+                    var results = [];
+                    for (var i = 0; i < messages.length; i++) {
+                        results = results.concat(messages[i][fieldname])
+                    }
+                    on_update(messages, results);
+                }
+            });
+        };
+
+        for (var start_at = 0; start_at < nvals; start_at = start_at + max_results) {
+            get_single(start_at, on_update, fieldname);
+        }
+    });
+}
+
+
+function get_jira_info(startAt, board_name, jql, restrict_fields, on_update) {
+    var jira = {};
+    jira.board_name = board_name;
+
+    base_url = window.location.href.toString().split('/').slice(0, 4).join('/')
+
+    jira_call(base_url + "/rest/jiraanywhere/1.0/servers", function(msg) {
+        AJS.$.each(msg, function(key, val) {
+            if (val.name && val.name.indexOf('Issue System') > -1) {
+                var jira_url = base_url + "/plugins/servlet/applinks/proxy?appId=" + val.id + "&path=" + val.url;
+
+                jira_call(jira_url + "/rest/greenhopper/1.0/rapidviews/viewsData", function(msg) {
+                    for (var i = 0; i < msg.views.length; i++) {
+                        if (msg.views[i].name === board_name) {
+                            jira.board = msg.views[i];
+                            break;
+                        }
+                    }
+                    jira_call(jira_url + "/rest/agile/1.0/board/" + jira.board.id + "/configuration", function(msg) {
+                        jira.board.config = msg;
+
+                        console.log("Board ID is " + jira.board);
+
+                        var prefix = "?"
+                        var query_url = jira_url + "/rest/agile/1.0/board/" + jira.board.id + "/backlog"
+                        if (jql != undefined)
+                        {
+                            query_url = query_url + prefix + "jql=" + encodeURIComponent(jql);
+                            prefix = "&";
+                        }
+                        if (restrict_fields != undefined)
+                        {
+                            
+                            query_url = query_url + prefix + "fields=" + restrict_fields.join(",");
+                            prefix = "&";
+                        }
+                        jira_get_all(query_url, "issues", function (messages, issues) {
+                            jira.issues = issues;
+                            jira.messages = messages;
+                            console.log(jira);
+                            on_update(jira);
+                        });
+                    });
+                });
+            }
+        });
+    });
+}
+
+function jira_moves_to_clipboard(rankCustomFieldId, moves) {
+    const el = document.createElement('textarea');
+    el.value = JSON.stringify([rankCustomFieldId].concat(moves));
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+}
+
+bookmarklet = String.raw`javascript: function do_moves(rankCustomFieldId, moves) {AJS.$.ajax({data: JSON.stringify({'issues': [moves[0][0]], 'rankBeforeIssue': moves[0][1], "rankCustomFieldId":rankCustomFieldId}), url: 'https://jira.dolby.net/jira/rest/agile/1.0/issue/rank', type: "PUT", async: true, datatype: 'json', contentType: 'application/json'}).done(function() {data = JSON.parse(this.data); console.log('Succeeded in moving ' + data.issues[0] + ' before ' + data.rankBeforeIssue); if (moves.length > 1) {do_moves(rankCustomFieldId, moves.slice(1))} else {alert('Success')}}).fail(function() {console.log(this); alert('Failed')})}; var reply = window.prompt("Paste your issue moves here: "); moves = JSON.parse(reply); if ((moves != null) && (moves != undefined) && (moves.length > 1) && (moves.slice(1).map(function (move) {return move.length == 2;})).reduce(function(a, b) {return a && b;})) {do_moves(moves[0], moves.slice(1))} else {alert("Invalid move syntax, must be [rankCustomFieldId, [issueToMove, aboveThisIssue], ...]")};`
